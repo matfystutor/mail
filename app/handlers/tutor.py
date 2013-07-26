56538c87b77fb8041f77f69bdace9ae67c59572e
Mathias Rav <rav@cs.au.dk>
2013-07-26 15:14:39 +0200

    Support sending to rusclasses.

--- app/handlers/tutor.py
+++ app/handlers/tutor.py
@@ -5,16 +5,19 @@ from config import settings
 from lamson import view, mail
 
 from django.contrib.auth.models import User
-from mftutor.tutor.models import TutorGroup
+from mftutor.tutor.models import TutorGroup, RusClass
 from mftutor.aliases.models import *
 from mftutor.activation.models import ProfileActivation
-from mftutor.settings import YEAR
+from mftutor.settings import YEAR, RUSCLASS_BASE, TUTORS_PREFIX
 
 @route("(address)@(host)", address=".+")
 @stateless
 def RELAY(message, **kwargs):
     logging.debug(u"Got a message for "+kwargs['address'].decode('utf-8')+u", To: "+unicode(message['to']))
     if relay_tutorgroup(message, **kwargs):
+        return RELAY
+
+    if relay_rusclass(message, **kwargs):
         return RELAY
 
     relay_unknown(message, **kwargs)
@@ -44,8 +47,69 @@ def relay_tutorgroup(message, address, host):
     logging.info("Message from "+message['from']+" to \""+address+"\" ("+str(len(emails))+" recipients): \""+message['subject']+"\"")
     if not emails:
         return False
+    logging.debug("Message: "+unicode(message))
     relay.deliver(message, To=emails)
     return True
+
+def relay_rusclass(message, address, host):
+    handle_base = {handle: []
+            for official, handle, internal in RUSCLASS_BASE}
+    handles = {}
+    for rusclass in RusClass.objects.filter(year__exact=YEAR):
+        handles[rusclass.handle] = rusclass
+        for h in handle_base.keys():
+            if rusclass.handle.startswith(h):
+                handle_base[h].append(rusclass)
+
+    handle = address
+
+    if address.startswith(TUTORS_PREFIX):
+        tutors_only = True
+        handle = address[len(TUTORS_PREFIX):]
+    else:
+        tutors_only = False
+
+    logging.debug("Address [{0}] is handle [{1}], tutors_only = {2}"
+            .format(address, handle, tutors_only))
+
+    if address in handles:
+        russes = [u.email for u in User.objects.filter(
+                tutorprofile__rus__year__exact=YEAR,
+                tutorprofile__rus__rusclass__handle__exact=address)]
+        tutors = [u.email for u in User.objects.filter(
+                tutorprofile__tutor__year__exact=YEAR,
+                tutorprofile__tutor__rusclass__handle__exact=address)]
+        tutors += [pa.email for pa in ProfileActivation.objects.filter(
+                profile__tutor__year__exact=YEAR,
+                profile__tutor__rusclass__handle__exact=address)]
+    elif address in handle_base:
+        russes = [u.email for u in User.objects.filter(
+                tutorprofile__rus__year__exact=YEAR,
+                tutorprofile__rus__rusclass__in=handle_base[address])]
+        tutors = [u.email for u in User.objects.filter(
+                tutorprofile__tutor__year__exact=YEAR,
+                tutorprofile__tutor__rusclass__in=handle_base[address])]
+        tutors += [pa.email for pa in ProfileActivation.objects.filter(
+                profile__tutor__year__exact=YEAR,
+                profile__tutor__rusclass__in=handle_base[address])]
+    else:
+        return False
+
+    if tutors_only:
+        russes = []
+
+    emails = russes + tutors
+    emails = [email for email in emails if '@' in email]
+
+    logging.debug("Address [{0}] goes to {1} russes, {2} tutors, in total {3} emails"
+            .format(address, len(russes), len(tutors), len(emails)))
+
+    if not emails:
+        return False
+
+    relay.deliver(message, To=emails)
+    return True
+
 
 def relay_unknown(message, address, host):
     """Let the admin know that the message could not be delivered."""
